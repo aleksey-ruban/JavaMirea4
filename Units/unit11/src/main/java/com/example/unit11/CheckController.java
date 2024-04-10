6987b0ad6bdc1fa97c7742c7b5f11445cb5ac903
aleksey-ruban <alekseyruban555@gmail.com>
2024-04-10 10:56:23 +0300

    Unit11

---
+++ Units/unit11/src/main/java/com/example/unit11/CheckController.java
@@ -0,0 +1,12 @@
+package com.example.unit11;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CheckController {
+    @GetMapping("/")
+    public String checkHealth() {
+        return "Application is up and running!";
+    }
+}
