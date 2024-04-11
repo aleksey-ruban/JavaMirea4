689759b6055ac2dcb630e66f0c077744a82c7493
aleksey-ruban <alekseyruban555@gmail.com>
2024-04-11 21:24:05 +0300

    Unit12

---
+++ Units/unit12/src/main/java/com/example/unit12/AppConfig.java
@@ -0,0 +1,14 @@
+package com.example.unit12;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public FileHashing fileHashing() {
+        String sourceFileName = "source.txt";
+        String targetFileName = "target.txt";
+        return new FileHashing(sourceFileName, targetFileName);
+    }
+}
---
+++ Units/unit12/src/test/java/com/example/unit12/Unit12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.unit12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Unit12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
