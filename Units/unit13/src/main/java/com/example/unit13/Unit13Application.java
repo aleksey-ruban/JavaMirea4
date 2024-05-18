5cd863e4e8506adf62f360d3cb629a4e6d84ead8
aleksey-ruban <alekseyruban555@gmail.com>
2024-05-18 14:30:43 +0300

    unit13

---
+++ Units/unit13/src/main/java/com/example/unit13/Unit13Application.java
@@ -0,0 +1,21 @@
+package com.example.unit13;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+public class Unit13Application {
+
+	public static void main(String[] args) {
+		ApplicationContext context = SpringApplication.run(Unit13Application.class, args);
+
+		Environment environment = context.getEnvironment();
+
+		System.out.println("Student Name: " + environment.getProperty("student.name"));
+		System.out.println("Student Last Name: " + environment.getProperty("student.last_name"));
+		System.out.println("Student Group: " + environment.getProperty("student.group"));
+	}
+
+}
